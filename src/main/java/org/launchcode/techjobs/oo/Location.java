137d9569a2379debe0bff26eab2b6646dfeedf4c
byui-programmer-96 <bam21002@byui.edu>
2024-10-19 18:40:39 -0500

    Initial commit

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -16,6 +16,17 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
+    public Location(String value) {
+        this();  // Call the empty constructor to initialize the 'id' field
+        this.value = value;
+    }
+
+    public Location(int id) {
+        this();
+        this.id = id;
+    }
+
+
 
     // Custom toString, equals, and hashCode methods:
 
