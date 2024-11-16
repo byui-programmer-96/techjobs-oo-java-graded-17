bb8794314e5715e74c30f3c161df2c7d620a580a
byui-programmer-96 <bam21002@byui.edu>
2024-11-16 16:52:01 -0600

    fixes

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -1,46 +1,7 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
-
 public class Employer extends JobField {
-
-    public Employer() {
-        super();
-    }
-
     public Employer(String value) {
         super(value);
     }
-
-    @Override
-    public String toString() {
-        return getValue();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Employer)) return false;
-        Employer employer = (Employer) o;
-        return getId() == employer.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
-
-    public int getId() {
-        return super.getId();
-    }
-
-    @Override
-    public String getValue() {
-        return super.getValue();
-    }
-
-    public void setValue(String value) {
-        super.setValue(value);
-    }
-
 }
