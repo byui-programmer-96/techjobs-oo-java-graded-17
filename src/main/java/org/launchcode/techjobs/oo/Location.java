56655d5689a16a94ae7c157d4a4b1b136633dc9b
byui-programmer-96 <bam21002@byui.edu>
2024-11-16 16:08:44 -0600

    fixes

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -2,10 +2,6 @@ package org.launchcode.techjobs.oo;
 
 public class Location extends JobField {
 
-    public Location() {
-        super();
-    }
-
     public Location(String value) {
         super(value);
     }
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,55 +1,7 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
-
 public class PositionType extends JobField {
-
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public PositionType() {
-        super ("");
-        id = nextId;
-        nextId++;
-    }
-
     public PositionType(String value) {
-        this();
-        this.value = value;
+        super(value);
     }
-
-
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PositionType)) return false;
-        PositionType that = (PositionType) o;
-        return getId() == that.getId();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getValue());
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
-
 }
