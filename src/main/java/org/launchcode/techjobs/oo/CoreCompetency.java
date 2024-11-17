21278f3a609b09b1e91283eed468ec9e26b5cd80
byui-programmer-96 <bam21002@byui.edu>
2024-11-17 15:42:09 -0600

    more fixes

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -1,18 +1,13 @@
 package org.launchcode.techjobs.oo;
 
-public class CoreCompetency extends JobField {
-    private String value;
+public class CoreCompetency {
+    private final String value;
 
     public CoreCompetency(String value) {
-        super(value);
         this.value = value;
     }
 
     public String getValue() {
-        return this.value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
+        return value;
     }
 }
