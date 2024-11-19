8659a0adf69ef98ef8b0c053d46f0e1cdd1e7bc8
byui-programmer-96 <bam21002@byui.edu>
2024-11-18 21:10:02 -0600

    new fixes

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -27,4 +27,15 @@ public abstract class JobField {
     public String toString() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // Check for reference equality
+        if (!(obj instanceof JobField other)) return false; // Check type and cast
+        return this.getId() == other.getId(); // Compare ids for equality
+    }
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(this.getId());
+    }
 }
